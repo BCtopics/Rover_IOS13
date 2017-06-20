350996388b438868b06832f417874ba9c91a4309
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-20 16:26:35 -0600

    Add baseURL, and URLForInfoForRover to BPGMarsRoverClient. Add isEqual to BPGRoverPhoto, and initWithDictionary functionality to BPGRoverPhoto Model.

--- Rover_IOS13/Rover-project-start/Sources/BPGMarsRoverClient.m
+++ Rover_IOS13/Rover-project-start/Sources/BPGMarsRoverClient.m
@@ -10,4 +10,35 @@
 
 @implementation BPGMarsRoverClient
 
++ (NSString *)apiKey {
+    static NSString *apiKey = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSURL *apiKeysURL = [[NSBundle mainBundle] URLForResource:@"APIKeys" withExtension:@"plist"];
+        if (!apiKeysURL) {
+            NSLog(@"Error! APIKeys file not found!");
+            return;
+        }
+        NSDictionary *apiKeys = [[NSDictionary alloc] initWithContentsOfURL:apiKeysURL];
+        apiKey = apiKeys[@"APIKey"];
+    });
+    return apiKey;
+}
+
++ (NSURL *)baseURL
+{
+    return [NSURL URLWithString:@"https://api.nasa.gov/mars-photos/api/v1"];
+}
+
++ (NSURL *)urlForInfoForRover:(NSString *)roverName
+{
+    NSURL *url = [self baseURL];
+    url = [url URLByAppendingPathComponent:@"manifests"];
+    url = [url URLByAppendingPathComponent:roverName];
+    
+    NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:url resolvingAgainstBaseURL:YES];
+    urlComponents.queryItems = @[[NSURLQueryItem queryItemWithName:@"api_key" value:[self apiKey]]];
+    return urlComponents.URL;
+}
+
 @end
--- Rover_IOS13/Rover-project-start/Sources/BPGRoverPhoto.m
+++ Rover_IOS13/Rover-project-start/Sources/BPGRoverPhoto.m
@@ -10,4 +10,45 @@
 
 @implementation BPGRoverPhoto
 
+- (BOOL)isEqual:(id)object
+{
+    if (!object || ![object isKindOfClass:[BPGRoverPhoto class]]) { return NO; }
+    BPGRoverPhoto *photo = object;
+    if (photo.photoIdentifier != self.photoIdentifier) { return NO; }
+    if (photo.solDateTaken != self.solDateTaken) { return NO; }
+    if (![photo.cameraName isEqualToString:self.cameraName]) { return NO; }
+    if (![photo.earthDate isEqualToDate:self.earthDate]) { return NO; }
+    return YES;
+}
+
++ (NSDateFormatter *)dateFormatter
+{
+    static NSDateFormatter *dateFormatter = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"];
+        dateFormatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+        dateFormatter.dateFormat = @"yyyy-MM-dd";
+    });
+    return dateFormatter;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super init];
+    if (self) {
+        
+        if (!dictionary[@"id"]) { return nil; }
+        _photoIdentifier = [dictionary[@"id"] integerValue];
+        _solDateTaken = [dictionary[@"sol"] integerValue];
+        _cameraName = dictionary[@"camera"][@"name"];
+        NSString *earthDateString = dictionary[@"earth_date"];
+        _earthDate = [[[self class] dateFormatter] dateFromString:earthDateString];
+        _imageURL = [NSURL URLWithString:dictionary[@"img_src"]];
+        if (!_imageURL) { return nil; }
+    }
+    return self;
+}
+
 @end
