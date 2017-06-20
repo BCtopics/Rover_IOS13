9e009765dc55e4068df9a62a1106d1b4748c9635
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-20 16:49:19 -0600

    Add BPGMarsPhotoDetailViewController, Design BPGMarsPhotoDetailViewController view

---
+++ Rover_IOS13/Rover-project-start/Sources/BPGMarsPhotoDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BPGMarsPhotoDetailViewController.h
+//  Rover
+//
+//  Created by Bradley GIlmore on 6/20/17.
+//  Copyright © 2017 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPGMarsPhotoDetailViewController : UIViewController
+
+@end
