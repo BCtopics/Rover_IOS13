355cb6e5bdafaaede78b56fd8a819ce6112b40fe
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-20 09:01:27 -0600

    Initial Project Creation / Cleanup

---
+++ Rover_IOS13/Rover_IOS13/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Rover_IOS13
+//
+//  Created by Bradley GIlmore on 6/20/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
